b6ec32e8bc08af3ad1997e90acd80e086e089908
Pedro2494 <antunes-pedro@hotmail.com>
2020-04-15 15:31:53 +0100

    Conclusao da janela principal e da ediçao dos seus componente conclusao da segunda ativity e seus componentes inicio da do codigo

---
+++ app/src/main/java/pt/ipg/healthmanagement/Dados_paciente.java
@@ -0,0 +1,14 @@
+package pt.ipg.healthmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dados_paciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_paciente);
+    }
+}
--- app/src/main/java/pt/ipg/healthmanagement/MainActivity.java
+++ app/src/main/java/pt/ipg/healthmanagement/MainActivity.java
@@ -2,7 +2,10 @@ package pt.ipg.healthmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void submeter(View view) {
+        Intent intent = new Intent(this, Dados_paciente.class);
+
+        EditText Nome = (EditText) findViewById(R.id.nome_paciente);
+        String nome_Paciente = Nome.getText().toString();
+        intent.putExtra("nome_Paciente", nome_Paciente);
+
+        EditText Idade =(EditText) findViewById(R.id.idade_paciente);
+        String idade_Paciente = Idade.getText().toString();
+        intent.putExtra("idade_Paciente", idade_Paciente);
+
+
+
+        startActivity(intent);
     }
 }
