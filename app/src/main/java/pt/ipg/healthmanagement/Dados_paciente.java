4d7a656c0915cbb38546781a90ae4205cb63f201
Pedro2494 <antunes-pedro@hotmail.com>
2020-04-19 22:58:15 +0100

    Conclusao da janela principal e da ediçao dos seus componente conclusao da segunda ativity e seus componentes ininicio da validaçao de dados nas respetivas ativities

--- app/src/main/java/pt/ipg/healthmanagement/Dados_paciente.java
+++ app/src/main/java/pt/ipg/healthmanagement/Dados_paciente.java
@@ -2,13 +2,66 @@ package pt.ipg.healthmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.widget.TextView;
+
+
+
 public class Dados_paciente extends AppCompatActivity {
+        TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dados_paciente);
+
+        Intent intent = getIntent();
+
+        String nome = intent.getStringExtra("nome_Paciente");
+        TextView Nome = (TextView) findViewById(R.id.textViewNome);
+        Nome.setText(nome);
+
+
+        String idade = intent.getStringExtra("idade_Paciente");
+        TextView Idade = (TextView) findViewById(R.id.textViewIdade);
+        Idade.setText(idade);
+
+        String datanascimento = intent.getStringExtra("data_de_nascimento");
+        TextView DataNascimento = (TextView) findViewById(R.id.textViewDataNas);
+        DataNascimento.setText(datanascimento);
+
+        String contactoemergengia = intent.getStringExtra("contacto_de_emergencia");
+        TextView ContactoEmergencia = (TextView) findViewById(R.id.textViewContacto);
+        ContactoEmergencia.setText(contactoemergengia);
+
+        String numerocc = intent.getStringExtra("numero_CC");
+        TextView NumeroCC = (TextView) findViewById(R.id.textViewcartao);
+        NumeroCC.setText(numerocc);
+
+        String nomesintoma = intent.getStringExtra("nome_Sintoma");
+        TextView NomeSintoma = (TextView) findViewById(R.id.textViewSintoma);
+        NomeSintoma.setText(nomesintoma);
+
+        String descricaosintoma = intent.getStringExtra("descricao_Sintoma");
+        TextView DescricaoSintoma = (TextView) findViewById(R.id.textViewSintomaDesc);
+        DescricaoSintoma.setText(descricaosintoma);
+
+        textView = (TextView) findViewById(R.id.textViewSexo);
+
+        StringBuilder str = new StringBuilder("");
+        if (intent.getBooleanExtra("Masculino", false)){
+            str.append("Masculino");
+        }
+
+        if (intent.getBooleanExtra("Feminino", false)){
+            str.append("Feminino");
+        }
+        if(str.length() == 0){
+            str.append("Plain");
+        }
+        textView.setText(str);
+
     }
 }
--- app/src/main/java/pt/ipg/healthmanagement/MainActivity.java
+++ app/src/main/java/pt/ipg/healthmanagement/MainActivity.java
@@ -2,17 +2,26 @@ package pt.ipg.healthmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox genM, genF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        genM = (CheckBox) findViewById(R.id.masculino);
+        genF =(CheckBox) findViewById(R.id.feminino);
     }
 
 
@@ -23,12 +32,90 @@ public class MainActivity extends AppCompatActivity {
         String nome_Paciente = Nome.getText().toString();
         intent.putExtra("nome_Paciente", nome_Paciente);
 
-        EditText Idade =(EditText) findViewById(R.id.idade_paciente);
+        EditText Idade = (EditText) findViewById(R.id.idade_paciente);
         String idade_Paciente = Idade.getText().toString();
         intent.putExtra("idade_Paciente", idade_Paciente);
 
 
+        EditText DataNascimento = (EditText) findViewById(R.id.data_nascimento);
+        String data_de_nascimento = DataNascimento.getText().toString();
+        intent.putExtra("data_de_nascimento", data_de_nascimento);
+
+        EditText ContactoEmergencia = (EditText) findViewById(R.id.contacto_emergencia);
+        String contacto_de_emergencia = ContactoEmergencia.getText().toString();
+        intent.putExtra("contacto_de_emergencia", contacto_de_emergencia);
+
+        EditText NumeroCC = (EditText) findViewById(R.id.numero_cc);
+        String numero_CC = NumeroCC.getText().toString();
+        intent.putExtra("numero_CC", numero_CC);
+
+        EditText NomeSintoma = (EditText) findViewById(R.id.sintomas);
+        String nome_Sintoma = NomeSintoma.getText().toString();
+        intent.putExtra("nome_Sintoma", nome_Sintoma);
+
+        EditText SintomaDescricao = (EditText) findViewById(R.id.descricao_sintomas);
+        String descricao_Sintoma = SintomaDescricao.getText().toString();
+        intent.putExtra("descricao_Sintoma", descricao_Sintoma);
+
+
+        //validar dados
+
+        if (nome_Paciente.length() <= 0) {
+            Nome.setError("Campo obrigatório");
+            Nome.requestFocus();
+            return;
+        }
+        if (nome_Paciente.length() <= 0) {
+            Nome.setError("Campo obrigatório");
+            Nome.requestFocus();
+            return;
+        }
+
+        if (idade_Paciente.length() <= 0) {
+            Idade.setError("Campo obrigatório");
+            Idade.requestFocus();
+            return;
+        }
+
+        if (data_de_nascimento.length() <= 0) {
+            DataNascimento.setError("Campo obrigatório");
+            DataNascimento.requestFocus();
+            return;
+        }
+
+        if (contacto_de_emergencia.length() <= 0) {
+            ContactoEmergencia.setError("Campo obrigatório");
+            ContactoEmergencia.requestFocus();
+            return;
+        }
+
+        if (numero_CC.length() <= 0) {
+            NumeroCC.setError("Campo obrigatório");
+            NumeroCC.requestFocus();
+            return;
+        }
+
+        if (nome_Sintoma.length() <= 0) {
+            NomeSintoma.setError("Campo obrigatório");
+            NomeSintoma.requestFocus();
+            return;
+        }
+
+        if (descricao_Sintoma.length() <= 0) {
+            SintomaDescricao.setError("Campo obrigatório");
+            SintomaDescricao.requestFocus();
+            return;
+        }
+
+        intent.putExtra("Masculino", genM.isChecked());
+        intent.putExtra("Feminino", genF.isChecked());
+
 
         startActivity(intent);
     }
+
+
+
 }
+
+
