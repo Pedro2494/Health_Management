97a3b0dcb8023be07ab9390e77db87b6cb77b85b
Pedro2494 <antunes-pedro@hotmail.com>
2020-04-23 00:52:57 +0100

    Conclusao da janela principal e da ediçao dos seus componente conclusao da segunda ativity e seus componentes ininicio da validaçao de dados nas respetivas ativities implementaçao de um spinner para passar dados para uma segunda activity

--- app/src/main/java/pt/ipg/healthmanagement/Dados_paciente.java
+++ app/src/main/java/pt/ipg/healthmanagement/Dados_paciente.java
@@ -12,35 +12,42 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Dados_paciente extends AppCompatActivity {
          //para checkbox
         TextView textView;
 
-        //para lista medicamentos
-        EditText et;
-        Button bt;
-        ListView lv;
-
-        ArrayList<String> arrayList;
-        ArrayAdapter<String> adapter;
-
+    //lista medicamentos
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
+    private EditText txtInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dados_paciente);
 
-        //para lista medicamentos
-        et = (EditText) findViewById(R.id.medicamento);
-        bt = (Button) findViewById(R.id.button_add_Data);
-        lv = (ListView) findViewById(R.id.ListaMedicamento);
+        //lista medicamentos
+        ListView listView =(ListView)findViewById(R.id.listv);
+        String[] items={""};
+        arrayList = new ArrayList<>(Arrays.asList(items));
+        adapter = new ArrayAdapter<String>(this,R.layout.list_item,R.id.txtitem,arrayList);
+        listView.setAdapter(adapter);
+        txtInput =(EditText)findViewById(R.id.txtinput);
+        Button btAdd=(Button)findViewById(R.id.btadd);
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newItem = txtInput.getText().toString();
+                arrayList.add(newItem);
+                adapter.notifyDataSetChanged();
+            }
+        });
 
-        arrayList = new ArrayList<String>();
-        adapter = new ArrayAdapter<String>(Dados_paciente.this, android.R.layout.simple_list_item_1, arrayList);
-        lv.setAdapter(adapter);
 
 
         //dados pessoais do paciente
@@ -106,11 +113,9 @@ public class Dados_paciente extends AppCompatActivity {
         estado.setText(data);
 
 
+
+
     }
-        //adiciona medicamento
-    public void adicionaMedicamento(View view) {
-       String result = et.getText().toString();
-       arrayList.add(result);
-       adapter.notifyDataSetChanged();
-    }
+
 }
+
